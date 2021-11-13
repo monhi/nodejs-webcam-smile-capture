94d524c22fefba19bcc824c89ae433091ba82158
Monhi <m.monhi@gmail.com>
2021-11-13 11:21:30 +0330

    Add files via upload

---
+++ server.js
@@ -0,0 +1,11 @@
+const   fs          = require("fs");
+const   path 		= require('path');
+const   express     = require("express");
+const   app         = express();
+const   PORT        = 3000;
+
+app.use(express.static('public'));
+
+app.listen(PORT,()=>{
+    console.log(`Server started on port ${PORT}`);
+});
